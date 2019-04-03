676733d70d83b6b724c6a338667dd571a6833a80
rokazu <rokazu.master@users.noreply.github.com>
2019-04-02 22:37:04 -0600

    cambio al ignore

---
+++ estructuras.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#ifndef _ESTRUCTURAS.H_
+#define _ESTRUCTURAS.H_
+
+typedef struct {
+	int id;
+	char *nombre;
+}libro;
+
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,10 @@
+
+#include "funciones.h"
+#include "estructuras.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	system("PAUSE");
+	return 0;
+}
